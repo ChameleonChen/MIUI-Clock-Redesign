7950c79f17b8d89dbec107b79a28961493d82a0c
ChameleonChen <colinchanwisdom@126.com>
2015-08-01 18:38:19 +0800

    rebuild start

--- app/src/main/java/com/chameleonchen/miui_clock/MainActivity.java
+++ app/src/main/java/com/chameleonchen/miui_clock/MainActivity.java
@@ -4,14 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
+
+    private ListView clockList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        clockList = (ListView) findViewById(R.id.lv_clock_list);
+        String[] data = new String[20];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = "item...." + i;
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, data);
+        clockList.setAdapter(adapter);
+
     }
 
     @Override
--- app/src/main/java/com/chameleonchen/miui_clock/views/ElasticAndAlignBottomLayout.java
+++ app/src/main/java/com/chameleonchen/miui_clock/views/ElasticAndAlignBottomLayout.java
@@ -80,7 +80,7 @@ public class ElasticAndAlignBottomLayout extends RelativeLayout{
     }
 
     private boolean isTouchDown = false;
-    private float lastY;
+    private float lastY = 0;
     private float dY;
     private float offset = 0;
     private int firstTop;
@@ -90,16 +90,38 @@ public class ElasticAndAlignBottomLayout extends RelativeLayout{
             return ;
         // reset values here.
         offset = 0;
+        lastY = 0;
     }
 
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        // in this method, this only accept ACTION_DOWN even.
         // it must decide whether to intercept the even.
 
-        // return true to intercept the even, and did not pass on to child view.
-        // The even will pass on to it's own onTouchEven() method.
-        // see more details: http://developer.android.com/intl/zh-cn/reference/android/view/ViewGroup.html#onInterceptTouchEvent(android.view.MotionEvent)
-        return true;
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_DOWN) {
+            isTouchDown = true;
+            initOriginalPositionData();
+            lastY = ev.getRawY();
+            firstTop = getTop();
+        }
+
+        // if the elastic view group move to top limit, did not intercept the even.
+        if (firstTop == mixDistanceToTop) {
+            if (action == MotionEvent.ACTION_MOVE) {
+                ElasticListView child = getChildView();
+                if (child != null) {
+                    if (child.isListViewAtTop()) {
+                    }
+                }
+            }
+            return false;
+        }
+        else
+            // return true to intercept the even, and did not pass on to child view.
+            // The even will pass on to it's own onTouchEven() method.
+            // see more details: http://developer.android.com/intl/zh-cn/reference/android/view/ViewGroup.html#onInterceptTouchEvent(android.view.MotionEvent)
+            return true;
     }
 
     @Override
@@ -109,13 +131,8 @@ public class ElasticAndAlignBottomLayout extends RelativeLayout{
         // If the even pass on here, means the even will not pass on to child view
 
         final int action = MotionEventCompat.getActionMasked(ev);
-        if (action == MotionEvent.ACTION_DOWN) {
-            isTouchDown = true;
-            initOriginalPositionData();
-            lastY = ev.getRawY();
-            firstTop = getTop();
-        }
-        else if (action == MotionEvent.ACTION_UP) {
+
+        if (action == MotionEvent.ACTION_UP) {
             int temp = (int) (firstTop + offset);
             int length = maxDistanceToTop - mixDistanceToTop;
             if (temp < mixDistanceToTop || temp < mixDistanceToTop + length/2) {
@@ -130,18 +147,14 @@ public class ElasticAndAlignBottomLayout extends RelativeLayout{
         else if (action == MotionEvent.ACTION_MOVE) {
             dY = ev.getRawY() - lastY;
             int temp = (int) (firstTop + offset + dY);
-            if (temp > mixDistanceToTop && temp < maxDistanceToTop) {
+            if (temp >= mixDistanceToTop && temp <= maxDistanceToTop) {
                 offset = offset + dY;
                 layout(originalLeft, temp, originalRight, originalBottom);
+            } else if (temp < mixDistanceToTop) {
             }
 
             lastY = ev.getRawY();
         }
-
-        View childView = getChildAt(0);
-        int top = childView.getTop();
-        int bottom = childView.getBottom();
-        Log.i("Elastic", "top: " + top + "; bottom: " + bottom + "; height: " + (bottom - top));
 
         // it must return true.
         return true;
@@ -149,17 +162,26 @@ public class ElasticAndAlignBottomLayout extends RelativeLayout{
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        ElasticListView childView = getChildView();
+        childView.layout(0, 0, r - l, b - t);   // make view fill this view group.
+    }
+
+    private ElasticListView mElasticListView;
+
+    private ElasticListView getChildView() {
         final int count = getChildCount();
         if (count == 0)
-            return ;
+            return null;
         else if (count == 1) {
             // if this view group has a view, resize the view.
             // the view must fill this view group.
 
-            View child = getChildAt(0);
-            if (child.getVisibility() != GONE) {
-                child.layout(0, 0, r - l, b - t);   // make view fill this view group.
+            mElasticListView = (ElasticListView) getChildAt(0);
+            if (mElasticListView.getVisibility() != GONE) {
+                return mElasticListView;
             }
+            else
+                return null;
         }
         else {
             throw new IllegalStateException("The ElasticAndAlignBottomLayout must only has zero or one view");
